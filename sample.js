6c2160d860bb5812a1e6964e75e4b6a76b9b9b86
srsuper <aot5310@gmail.com>
2020-09-26 17:31:00 +0700

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,7 +1,7 @@
 var videoToFacebookLive = require('./index.js');
 
-var rtmp_uri = "rtmp://live-api-s.facebook.com:80/rtmp/";
-var stream_key = "10217959536199143?ds=1&s_sw=0&s_vt=api-s&a=AbwYabf-tuYcrKqS";
+var rtmp_uri = "rtmps://live-api-s.facebook.com:443/rtmp/";
+var stream_key = "242410760472083?s_bl=1&s_ps=1&s_psm=1&s_sw=0&s_vt=api-s&a=Abx22BF4BFhrbZOO";
 var video_file = ".videos/big_buck_bunny.mp4";
 
 
